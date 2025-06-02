07fe19a6a7ac2589b99036eb5f60c4c896491262
llllyyyynnnn <157394687+llllyyyynnnn@users.noreply.github.com>
2025-06-02 12:28:15 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace OOPC_VT25_Group_15_2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
