c4003f90cdc4e9a571a3f67d9ee9126bf8ae429e
llllyyyynnnn <157394687+llllyyyynnnn@users.noreply.github.com>
2025-06-11 17:40:00 +0200

    Data annotations

--- DataManager/Entities.cs
+++ DataManager/Entities.cs
@@ -6,61 +6,61 @@ namespace DataManager
 {
     public class Entity
     {
-        [Key] public int id { get; set; }
+        [Required] [Key] public int id { get; set; }
     }
 
     public class Entities
     {
         public class Member : Entity
         {
-            public required string firstName { get; set; }
-            public required string lastName { get; set; }
-            public required string mailAddress { get; set; }
-            public required string phoneNumber { get; set; }
-            public required DateTime birthDate { get; set; }
-            public required string pinCode { get; set; }
+            [Required] [MaxLength(64)] public required string firstName { get; set; }
+            [Required] [MaxLength(64)] public required string lastName { get; set; }
+            [Required] [MaxLength(128)] public required string mailAddress { get; set; }
+            [Required] [MaxLength(24)] public required string phoneNumber { get; set; }
+            [Required] public required DateTime birthDate { get; set; }
+            [Required] [MaxLength(100)] public required string pinCode { get; set; }
         }
 
         public class Coach : Entity
         {
-            public required string firstName { get; set; }
-            public required string lastName { get; set; }
-            public required string mailAddress { get; set; }
-            public required string phoneNumber { get; set; }
+            [Required][MaxLength(64)] public required string firstName { get; set; }
+            [Required][MaxLength(64)] public required string lastName { get; set; }
+            [Required][MaxLength(128)] public required string mailAddress { get; set; }
+            [Required][MaxLength(24)] public required string phoneNumber { get; set; }
+            [Required] public required DateTime birthDate { get; set; }
+            [Required][MaxLength(100)] public required string pinCode { get; set; }
             public required string specialisation { get; set; }
-            public required DateTime birthDate { get; set; }
-            public required string pinCode { get; set; }
         }
 
         public class Session : Entity
         {
-            public required string name { get; set; }
-            public required DateTime startTime { get; set; }
-            public required DateTime endTime { get; set; }
-            public required string location { get; set; }
-            public required Member member { get; set; }
-            public required Coach coach { get; set; }
+            [Required][MaxLength(64)] public required string name { get; set; }
+            [Required] public required DateTime startTime { get; set; }
+            [Required] public required DateTime endTime { get; set; }
+            [Required][MaxLength(64)] public required string location { get; set; }
+            [Required] public required Member member { get; set; }
+            [Required] public required Coach coach { get; set; }
         }
 
         public class Category : Entity
         {
-            public required string name { get; set; }
+            [Required][MaxLength(64)] public required string name { get; set; }
         }
 
         public class Gear : Entity
         {
-            public required string name { get; set; }
-            public required Category category { get; set; }
-            public required string condition { get; set; }
-            public required bool available { get; set; }
+            [Required][MaxLength(64)] public required string name { get; set; }
+            [Required] public required Category category { get; set; }
+            [Required][MaxLength(64)] public required string condition { get; set; }
+            [Required] public required bool available { get; set; }
         }
 
         public class GearLoan : Entity
         {
-            public required Gear gear { get; set; }
-            public required Member loanOwner { get; set; }
-            public required DateTime loanDate { get; set; }
-            public required DateTime returnDate { get; set; }
+            [Required] public required Gear gear { get; set; }
+            [Required] public required Member loanOwner { get; set; }
+            [Required] public required DateTime loanDate { get; set; }
+            [Required] public required DateTime returnDate { get; set; }
         }
     }
 }
